1acb1ab422ad0053173764646d7481296d5334d2
Dhruvil Jain <dhruvil.jain2021@vitbhopal.ac.in>
2023-01-06 18:46:02 +0530

    path added

--- vlance/urls.py
+++ vlance/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('landing/' , views.landing, name='landing')
 
 
 ]
