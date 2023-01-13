b864f809dfc40a746f700c086ce1cf32d2ebab21
Dhruvil Jain <dhruvil.jain2021@vitbhopal.ac.in>
2023-01-13 13:25:42 +0530

    index page card

--- vlance/models.py
+++ vlance/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Jobs(models.Model):
-    task_Name = models.CharField(max_length=50 , blank = False)
+    task_name = models.CharField(max_length=50 , blank = False)
     amount = models.IntegerField( default = 0,blank = False)
     description = models.TextField(max_length=500 , blank = False, default = None)
-    date_posted = models.DateField(auto_now_add=True)
+    date_posted = models.DateField(blank= False)
     email = models.EmailField(verbose_name="Email", blank = False)
     provided_by = models.CharField(max_length=50 , blank = False)
     deadline = models.DateField(blank = False)
@@ -14,5 +14,5 @@ class Jobs(models.Model):
     location = models.CharField(max_length=50 , blank = False)
 
     def __str__(self):
-        return f"{self.task_Name} - {self.amount} - {self.description} - {self.date_posted} - {self.email} - {self.provided_by} - {self.deadline} - {self.is_completed} - {self.location}"
+        return f"{self.task_name} - {self.amount} - {self.description} - {self.date_posted} - {self.email} - {self.provided_by} - {self.deadline} - {self.is_completed} - {self.location}"
 
