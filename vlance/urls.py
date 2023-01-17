47f0734bec393f32b91db172ea4326f502558d4d
Dhruvil Jain <dhruvil.jain2021@vitbhopal.ac.in>
2023-01-17 23:10:04 +0530

    login code started

--- vlance/urls.py
+++ vlance/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('landing/' , views.landing, name='landing'),
-    path('<str:job_id>' , views.jobs, name='jobs')
+    path('index/', views.index, name='index'),
+    path('' , views.landing, name='landing'),
+    path('<str:job_id>' , views.jobs, name='jobs'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout')
     
 ]
--- vlance/views.py
+++ vlance/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import render
 from .models import Jobs
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -13,5 +16,26 @@ def landing(request):
 def jobs(request, job_id):
     jobs = Jobs.objects.get(id=job_id)
     return render(request, "vlance/job.html", {
-        "jobs": jobs
+        "jobs": jobs,
+        "works": Jobs.objects.all()
     })    
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request , username = username , password = password)
+        if username is not None:
+            login(request , user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request , "vlance/login.html" , {
+                "message" : "Invalid Credentials"
+            })
+    else:
+        return render(request, "vlance/login.html")        
+
+def logout_view(request):
+    logout(request)
+    return render(request, "vlance/landing.html")
+         
